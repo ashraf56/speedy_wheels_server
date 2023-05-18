a7a71894db6274b25e14a0fd44851693c408c60d
ashraf56 <ashrafulfahim07@gmail.com>
2023-05-18 11:57:27 +0600

    server setup

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT|| 3000
+let cors=require('cors')
+require('dotenv').config()
+
+
+
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+  res.send('toy World!')
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
