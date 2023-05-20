addd001525aeaa49391b15c69bc827662caff1d9
ashraf56 <ashrafulfahim07@gmail.com>
2023-05-20 18:19:19 +0600

    find all data from MOngodb

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT|| 3000
 let cors=require('cors')
 
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
@@ -30,7 +30,6 @@ async function run() {
     let toyCollection=client.db('ToysDB').collection('Alltoy')
 
 app.post('/alltoy',async (req,res)=>{
-
 let toys=req.body;
 let result= await toyCollection.insertOne(toys)
 res.send(result)
@@ -40,6 +39,24 @@ app.get('/alltoy' , async(req,res)=>{
 
 let result= await  toyCollection.find().toArray();
 res.send(result)
+})
+app.get('/alltoy/:id',async(req,res)=>{
+
+   let toyid= req.params.id;
+    let query={_id: new ObjectId(toyid)}
+        let results= await toyCollection.findOne(query)
+        res.send(results)
+
+})
+
+app.get('/mytoy',async (req ,res)=>{
+
+let toyquery={}
+if (req.query?.email) {
+  toyquery={email: req.query.email}
+}
+let results= await toyCollection.find(toyquery).toArray()
+res.send(results)
 })
 
 
